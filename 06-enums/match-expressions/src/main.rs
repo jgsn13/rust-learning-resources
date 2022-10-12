70f2480c0082da44efec3ddfd539fe8e9672ec86
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-10-12 15:30:18 -0300

    feat: add match expression

---
+++ 06-enums/match-expressions/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    // ...
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
